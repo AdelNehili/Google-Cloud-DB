00092d6d9a30dfa57964760032b11f1bbf4b9dcc
AdelNehili <49352113+AdelNehili@users.noreply.github.com>
2022-12-12 17:53:21 +0100

    Add files via upload

---
+++ First try MySQL Python/connection_requirement.py
@@ -0,0 +1,4 @@
+current_user = 'root'
+current_password = 'root'
+current_host = '34.79.200.220'
+current_database = 'my_db'
---
+++ First try MySQL Python/googleCloudSLQ.py
@@ -0,0 +1,81 @@
+######## Program Begins Here ########
+
+#### Import Python libraries ####
+import sqlite3
+from google.cloud import storage
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import mysql.connector
+import sys
+
+#### details library ####
+from connection_requirement import *
+
+#### Establish Connetion ####
+cnx = mysql.connector.connect(user = current_user, password=current_password, host=current_host, 
+                              database=current_database)
+cursor = cnx.cursor()
+
+
+#### Connetion Established ####
+
+#### Execute query ####
+
+#query1 = ("select * from customer")
+query1 = ("show databases")
+cursor.execute(query1)
+frame = pd.DataFrame(cursor.fetchall())
+frame.head()
+
+"""
+#### Create dataframe from resultant table ####
+frame = pd.DataFrame(cursor.fetchall())
+
+frame.head()
+
+#### Put columns names for the dataframe ####
+
+frame.columns = [['customer_id', 'age', 'gender', 'home_airport', 'ticket_num', 'passport_num', 'first_name', 'last_name', 
+                 'email', 'cust_profile', 'tel']]
+
+frame.head()
+
+#### Inserting new records into the SQL table ####
+
+new_cust = pd.read_csv("customers2.csv")
+
+#### Inserting new records into the SQL table through For Loop ####
+
+temp = 0
+for i in range(len(new_cust)):
+    customer_id = str(new_cust['customer_id'].iloc[i])
+    age = int(new_cust['age'].iloc[i])
+    gender = str(new_cust['gender'].iloc[i])
+    home_airport = int(new_cust['home_airport'].iloc[i])
+    ticket_num = str(new_cust['ticket_num'].iloc[i])
+    passport_num = str(new_cust['passport_num'].iloc[i])
+    first_name = str(new_cust['first_name'].iloc[i])
+    last_name = str(new_cust['last_name'].iloc[i])
+    email_addr = str(new_cust['email'].iloc[i])
+    cust_profile = str(new_cust['cust_profile'].iloc[i])
+    tel = str(new_cust['tel'].iloc[i])
+    
+    query2 = ("INSERT INTO customer (customer_id, age, gender, home_airport, ticket_num, passport_num, first_name,"
+              " last_name, email_addr, tel, cust_profile)"
+              "values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+    val = (customer_id, age, gender, home_airport, ticket_num, passport_num, first_name, last_name, email_addr, tel, 
+           cust_profile)
+    cursor.execute(query2, val)
+    cnx.commit()
+    temp = temp + 1
+    print(temp, "Record Inserted for ",first_name )
+
+"""
+
+#### Extremely Important: Close your SQL connection ####
+
+cursor.close()
+cnx.close()
+
+######## Program Ends Here ########
